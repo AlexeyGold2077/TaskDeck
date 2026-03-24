5125ee66fe21ed66ddd8977844c257632c4d6ee9
Alexey <alexey@Alexeys-MacBook-Pro.local>
2026-03-24 15:53:25 +0300

    initialize empty project

---
+++ src/main/java/com/alexeygold2077/taskdeck/TaskdeckApplication.java
@@ -0,0 +1,13 @@
+package com.alexeygold2077.taskdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexeygold2077/taskdeck/TaskdeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexeygold2077.taskdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
